8b02d32c997b5c0df59d0bf8b6fd415f677e9ab0
Chloe Foronda <caforonda103@yahoo.com>
2019-12-21 14:09:24 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -22,7 +22,8 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  
+  the_office = ["Jim", "Dwight", "Pam"]
+  the_office.shift
 end
 
 def retrieve_element_from_index(array, index_number)
