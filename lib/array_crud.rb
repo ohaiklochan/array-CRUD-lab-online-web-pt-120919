e542818242553eaae112f47c309e2dde4bc46de5
Chloe Foronda <caforonda103@yahoo.com>
2019-12-21 14:31:58 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,42 @@
 def create_an_empty_array
-  
+  array = []
 end
 
 def create_an_array
-  
+  the_office = ["Michael","Jim", "Dwight", "Pam"]
 end
 
-def add_element_to_end_of_array(array, element)
-  
+def add_element_to_end_of_array
+  the_office = ["Michael","Jim", "Dwight", "Pam"]
+  the_office << "Ryan"
 end
 
-def add_element_to_start_of_array(array, element)
-  
+def add_element_to_start_of_array
+  the_office = ["Michael","Jim", "Dwight", "Pam"]
+  the_office.unshift("Ryan")
 end
 
-def remove_element_from_end_of_array(array)
-  
+def remove_element_from_end_of_array
+  the_office = ["Michael","Jim", "Dwight", "Pam"]
+  "Pam" = the_office.pop
 end
 
-def remove_element_from_start_of_array(array)
-  
+def remove_element_from_start_of_array
+  the_office = ["Michael","Jim", "Dwight", "Pam"]
+  "Michael" = the_office.shift
 end
 
-def retrieve_element_from_index(array, index_number)
-  
+def retrieve_element_from_index
+  the_office = ["Michael","Jim", "Dwight", "Pam"]
+  the_office[2]
 end
 
-def retrieve_first_element_from_array(array)
-  
+def retrieve_first_element_from_array
+  the_office = ["Michael","Jim", "Dwight", "Pam"]
+  the_office.first
 end
 
-def retrieve_last_element_from_array(array)
-  
+def retrieve_last_element_from_array
+  the_office = ["Michael","Jim", "Dwight", "Pam"]
+  the_office.last
 end
