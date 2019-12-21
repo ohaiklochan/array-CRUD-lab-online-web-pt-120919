0f0cb4e08af569b22119011f25f512b6bb1e6908
Chloe Foronda <caforonda103@yahoo.com>
2019-12-21 14:08:35 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,7 +17,8 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  
+  the_office = ["Jim", "Dwight", "Pam"]
+  the_office.pop
 end
 
 def remove_element_from_start_of_array(array)
