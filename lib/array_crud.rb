031f1e59c33e43a2880d78632035cfbb0afe92bb
Chloe Foronda <caforonda103@yahoo.com>
2019-12-21 14:30:51 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,12 +18,12 @@ end
 
 def remove_element_from_end_of_array
   the_office = ["Michael","Jim", "Dwight", "Pam"]
-  "Pam" = the_office.pop
+  the_office.pop
 end
 
 def remove_element_from_start_of_array
   the_office = ["Michael","Jim", "Dwight", "Pam"]
-  "Michael" = the_office.shift
+  the_office.shift
 end
 
 def retrieve_element_from_index
