1246ad72a2183f3051d4d2b5216b87e51c45adf8
Chloe Foronda <caforonda103@yahoo.com>
2019-12-21 14:22:04 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,7 +9,7 @@ end
 def add_element_to_end_of_array(array, element)
   the_office = ["Michael","Jim", "Dwight", "Pam"]
   the_office << "Ryan"
-  puts 
+  puts the_office.inspect
 end
 
 def add_element_to_start_of_array(array, element)
