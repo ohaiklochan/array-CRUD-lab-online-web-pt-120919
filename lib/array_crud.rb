01a78a46f655057940c170a796c197423bba309f
Chloe Foronda <caforonda103@yahoo.com>
2019-12-21 14:24:24 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -16,27 +16,27 @@ def add_element_to_start_of_array
   the_office.unshift("Ryan")
 end
 
-def remove_element_from_end_of_array(array)
+def remove_element_from_end_of_array
   the_office = ["Michael","Jim", "Dwight", "Pam"]
   the_office.pop
 end
 
-def remove_element_from_start_of_array(array)
+def remove_element_from_start_of_array
   the_office = ["Michael","Jim", "Dwight", "Pam"]
   the_office.shift
 end
 
-def retrieve_element_from_index(array, index_number)
+def retrieve_element_from_index
   the_office = ["Michael","Jim", "Dwight", "Pam"]
   the_office[2]
 end
 
-def retrieve_first_element_from_array(array)
+def retrieve_first_element_from_array
   the_office = ["Michael","Jim", "Dwight", "Pam"]
   the_office.first
 end
 
-def retrieve_last_element_from_array(array)
+def retrieve_last_element_from_array
   the_office = ["Michael","Jim", "Dwight", "Pam"]
   the_office.last
 end
