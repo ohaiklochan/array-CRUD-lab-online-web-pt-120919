0d79cf8fedeaa417c9fa0cb32571927cf7eee44e
Chloe Foronda <caforonda103@yahoo.com>
2019-12-21 14:23:33 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,12 +8,12 @@ end
 
 def add_element_to_end_of_array(array, element)
   the_office = ["Michael","Jim", "Dwight", "Pam"]
-  the_office.unshift "Ryan"
+  the_office << "Ryan"
 end
 
 def add_element_to_start_of_array(array, element)
   the_office = ["Michael","Jim", "Dwight", "Pam"]
-  the_office.push("Ryan")
+  the_office.unshift("Ryan")
 end
 
 def remove_element_from_end_of_array(array)
